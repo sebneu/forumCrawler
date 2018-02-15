2509758a8e09e83c05d69ef22838d7c995e88ba9
neumaier <sebastian.neumaier@wu.ac.at>
2018-02-15 13:24:13 +0100

    krone update

--- krone_crawler.py
+++ krone_crawler.py
@@ -21,15 +21,18 @@ class Crawler:
         self.browser.quit()
 
     def load_more_postings(self):
-        loadmore = self.browser.find_element_by_id("comment-list")
-        more_button = loadmore.find_element_by_class_name('next')
-        self.browser.execute_script("arguments[0].click();", more_button)
+        try:
+            loadmore = self.browser.find_element_by_id("comment-list")
+            more_button = loadmore.find_element_by_class_name('next')
+            self.browser.execute_script("arguments[0].click();", more_button)
 
-        selected_page = loadmore.find_element_by_class_name("selected").text
-        page_number = loadmore.find_element_by_class_name("commentList__maxCount").text.split(' ')[1]
-        if selected_page != page_number:
-            return True
-        else:
+            selected_page = loadmore.find_element_by_class_name("selected").text
+            page_number = loadmore.find_element_by_class_name("commentList__maxCount").text.split(' ')[1]
+            if selected_page != page_number:
+                return True
+            else:
+                return False
+        except:
             return False
 
 
@@ -63,57 +66,61 @@ class Crawler:
         articles_collection = db.articles
         postings_collection = db.postings
 
-        page = 'http://www.krone.at/' + article_id
-        article = {'_id': article_id, 'url': page}
-        try:
-            if politeness > 0:
-                time.sleep(politeness)
-            self.browser.get(page)
+        if not articles_collection.find_one({'_id': article_id}):
+            page = 'http://www.krone.at/' + article_id
+            article = {'_id': article_id, 'url': page}
+            try:
+                if politeness > 0:
+                    time.sleep(politeness)
+                self.browser.get(page)
 
-            # get article metadata
-            a_center = self.browser.find_element_by_class_name('col-xs-8')
-            a_datetime = a_center.find_element_by_class_name('c_pretitle').find_element_by_class_name('c_time').text.encode('utf-8')
-            article['date'] = dateparser.parse(a_datetime, languages=['de'])
+                # get article metadata
+                a_center = self.browser.find_element_by_class_name('col-xs-8')
+                a_datetime = a_center.find_element_by_class_name('c_pretitle').find_element_by_class_name('c_time').text.encode('utf-8')
+                article['date'] = dateparser.parse(a_datetime, languages=['de'])
 
-            a_title = a_center.find_element_by_class_name('c_title').find_element_by_tag_name('h1').text.encode('utf-8').strip()
-            article['title'] = a_title
+                a_title = a_center.find_element_by_class_name('c_title').find_element_by_tag_name('h1').text.encode('utf-8').strip()
+                article['title'] = a_title
 
-            a_topic = self.browser.find_element_by_class_name('c_active').find_element_by_tag_name('a').get_attribute('href').strip()
-            article['topic'] = a_topic
+                a_topic = self.browser.find_element_by_class_name('c_active').find_element_by_tag_name('a').get_attribute('href').strip()
+                article['topic'] = a_topic
 
-            articles_collection.insert(article)
+                articles_collection.insert(article)
 
-            postings = []
-            for p in self.get_postings_from_html(self.browser.page_source, article_id):
-                postings.append(p)
-
-            while self.load_more_postings():
+                postings = []
                 for p in self.get_postings_from_html(self.browser.page_source, article_id):
                     postings.append(p)
 
-            postings_collection.insert_many(postings)
-        except Exception as e:
-            logging.debug('Exception for article: ' + page)
-            logging.debug(e)
+                while self.load_more_postings():
+                    for p in self.get_postings_from_html(self.browser.page_source, article_id):
+                        postings.append(p)
+                if postings:
+                   postings_collection.insert_many(postings)
+            except Exception as e:
+                logging.debug('Exception for article: ' + page)
+                logging.debug(e)
 
 
 def get_links_from_doc(doc='krone_links.txt'):
     idset = set()
     with open(doc) as f:
         for line in f:
-            id = line.split('www.krone.at/')[1].strip()
-            if id.isdigit():
-                idset.add(id)
+            parts = line.split('www.krone.at/')
+            if len(parts) > 1:
+                id = parts[1].strip()
+                if id.isdigit():
+                    idset.add(id)
     with open('krone_links_cleaned.txt', 'w') as f:
         for id in idset:
             f.write(id + '\n')
 
 
 if __name__ == '__main__':
-    c = Crawler()
+    #c = Crawler()
     #with open('krone_links_cleaned.txt') as f:
     #    for code in f:
     #        c.get_postings(code.strip(), 1)
     #c = Crawler()
     #c.archive_articles()
-    c.get_postings(None, "1611934", politeness=1)
+    #c.get_postings(None, "1611934", politeness=1)
+    get_links_from_doc()
--- web_archive_crawler.py
+++ web_archive_crawler.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import time
 
+from bs4 import BeautifulSoup
+
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -26,29 +28,42 @@ class Crawler:
             if b.text == until:
                 break
         yield until
-        #
+
+
+    def get_urls_from_html(self, formatted_result):
+        soup = BeautifulSoup(formatted_result, 'html.parser')
+        circle = soup.find('g', id='d3_container')
+        res = set()
+        for link in circle.find_all('a'):
+            if link.has_attr('xlink:href'):
+                res.add(link['xlink:href'])
+            elif link.has_attr('href'):
+                res.add(link['href'])
+        return res
 
     def get_articles(self, site):
         waybackMachine = 'https://web.archive.org/web/sitemap/' + site
         self.browser.get(waybackMachine)
         all_links = set()
-
         time.sleep(150)
-        for y in self.iterYears(until='2015'):
+
+        years_iter = self.iterYears(until='2015')
+        for y in years_iter:
             print 'year: ', y
-            circle = self.browser.find_element_by_id("d3_container")
-            for path in circle.find_elements_by_tag_name('path'):
-                try:
-                    hover = ActionChains(self.browser).move_to_element(path)
-                    hover.perform()
-                    el = self.browser.find_element_by_class_name('sequence')
-                    link_el = el.find_element_by_tag_name('a')
-                    if link_el:
-                        text = link_el.get_attribute('href')
-                        all_links.add(text)
-                        print text
-                except Exception:
-                    print 'element not displayed'
+            all_links |= self.get_urls_from_html(self.browser.page_source)
+            #circle = self.browser.find_element_by_id("d3_container")
+            #for path in circle.find_elements_by_tag_name('a'):
+                #try:
+                    #hover = ActionChains(self.browser).move_to_element(path)
+                    #hover.perform()
+                    #el = self.browser.find_element_by_class_name('sequence')
+                    #link_el = el.find_element_by_tag_name('a')
+                    #if link_el:
+                    #    text = link_el.get_attribute('href')
+                    #    all_links.add(text)
+                    #    print text
+                #except Exception:
+                #    print 'element not displayed'
 
         return all_links
 
@@ -59,10 +74,10 @@ if __name__ == '__main__':
     #with open('presse_links_cleaned.txt') as f:
     #    for code in f:
     #        c.get_postings(code.strip(), 1)
-    # http://www.krone.at/
+    #
     c = Crawler()
-    articles = c.get_articles(site='https://diepresse.com/home/')
-    with open('presse_links.txt', 'w') as f:
+    articles = c.get_articles(site='http://www.krone.at/')
+    with open('krone_links.txt', 'w') as f:
         for l in articles:
             f.write(l)
             f.write('\n')
